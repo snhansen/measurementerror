6226311653b2e56cf22f2a4786767ecc3600adb8
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2022-11-03 11:31:24 +0100

    Fixed error

--- server.R
+++ server.R
@@ -85,8 +85,8 @@ server <- function(input, output) {
       new_dat <- dat()
       which_x <- input$xrange[1] <= new_dat$x & new_dat$x <= input$xrange[2]
       which_y <- input$yrange[1] <= new_dat$y & new_dat$y <= input$yrange[2]
-      new_dat$x[which_y] <- new_dat$x[which_y] + rnorm(sum(which_y), input$error_mean_x, input$error_sd_x, 0)
-      new_dat$y[which_x] <- new_dat$y[which_x] + rnorm(sum(which_x), input$error_mean_y, input$error_sd_y, 0)
+      new_dat$x[which_y] <- new_dat$x[which_y] + rnorm(sum(which_y), input$error_mean_x, input$error_sd_x)
+      new_dat$y[which_x] <- new_dat$y[which_x] + rnorm(sum(which_x), input$error_mean_y, input$error_sd_y)
       new_dat
     }
     else {
